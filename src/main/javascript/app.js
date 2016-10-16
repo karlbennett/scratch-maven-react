7550c7be80b13e7f9a5044f434f8d89a324e43c0
Karl Bennett <karl.bennett.smt@gmail.com>
2016-10-16 23:38:01 +1300

    Added some comments.

--- src/main/javascript/app.js
+++ src/main/javascript/app.js
@@ -9,10 +9,15 @@ import {Router, Route, browserHistory} from "react-router";
 import HelloWorldReducers from "./HelloWorldReducer.js";
 import HelloWorldContainer from "./HelloWorldContainer.js";
 
+// This will cause Webpack to add the compile "main.scss" SASS file to the index.html page.
 require('../sass/main.scss');
 
+// Here we create the Redux store and add the "redux-thunk" middleware that allows us to make asynchronous dispatches.
 let store = createStore(HelloWorldReducers, applyMiddleware(reduxThunk));
 
+// Here we setup the entire React app. First the "Provider" component is used to automatically add the Redux store to
+// all React components. Then the "Router" is used to setup the supported paths to the main component
+// e.g. "/" and "/helloWorld"
 window.app = ReactDOM.render(
     <Provider store={store}>
         <Router history={browserHistory}>
