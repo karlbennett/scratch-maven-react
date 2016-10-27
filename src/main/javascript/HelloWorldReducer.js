702d6d29c9b427281622f52b675df7ade2ec40a8
Karl Bennett <karl.bennett.smt@gmail.com>
2016-10-27 19:32:39 +1300

    Added copyright.

--- src/main/javascript/HelloWorldReducer.js
+++ src/main/javascript/HelloWorldReducer.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Karl Bennett
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Here we are defining the "Hello World" reducer, it is responsible for taking the "text" field from it's supplied
 // "action" and producing a new Redux "state" that contains that new "text" field. Doing this will cause Redux to
 // re-render any registered React components that are bound to the states "text" field. You can see one of these
--- src/main/javascript/HelloWorldService.js
+++ src/main/javascript/HelloWorldService.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Karl Bennett
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // This is the service that makes the "Hello World" HTTP request. It is completely decouple from any React or Redux
 // code, this makes it much simpler to unit test.
 export default class HelloWorldService {
