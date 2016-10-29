48218b82b0f6ba2f381d605812b9f4f42c1ca6b6
Karl Bennett <karl.bennett.smt@gmail.com>
2016-10-30 11:39:11 +1300

    Improved the Spring Security responses and disabled the CSRF protection to allow custom login requests.

--- src/main/java/scratch/Application.java
+++ src/main/java/scratch/Application.java
@@ -6,9 +6,16 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 import static javax.servlet.http.HttpServletResponse.SC_OK;
 import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
@@ -34,14 +41,15 @@ public class Application extends WebSecurityConfigurerAdapter {
         return "Hello world.";
     }
 
-    @RequestMapping(path = "secure", method = GET, produces = APPLICATION_JSON_VALUE)
-    public String secure() {
+    @RequestMapping(path = "secret", method = GET, produces = APPLICATION_JSON_VALUE)
+    public String secret() {
         return "Hello secret world.";
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf().disable()
             .authorizeRequests().antMatchers("/", "/scripts/*", "/images/*", "/styles/*", "/hello").permitAll()
             .anyRequest().authenticated()
             .and()
@@ -52,7 +60,9 @@ public class Application extends WebSecurityConfigurerAdapter {
             .failureHandler((request, response, exception) -> response.sendError(SC_UNAUTHORIZED, "Login Failed"))
             .loginPage("/login").permitAll()
             .and()
-            .logout().logoutUrl("/logout").permitAll();
+            .logout()
+            .logoutSuccessHandler((request, response, authentication) -> response.setStatus(SC_OK))
+            .logoutUrl("/logout").permitAll();
     }
 
     @Override
